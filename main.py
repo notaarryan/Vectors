bfc4a9693e0c1139af0635008a62ebac9a6785e3
notaarryan <aryanparmar2503@gmail.com>
2024-10-14 21:02:59 -0400

    plot function added to 3d vector

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 
 class VectorOperations:
     pass
@@ -18,3 +19,25 @@ class TwoDVector(VectorOperations):
 
     def mag(self):
         return ((self.i**2)+(self.j**2))**0.5
+    
+class ThreeDVector(TwoDVector):
+    def __init__(self, i, j, k):
+        super().__init__(i, j)
+        self.k = k
+        self.vector = [i,j,k]
+        self.xpoint = np.array([0,i])
+        self.ypoint = np.array([0,j])
+        self.zpoint = np.array([0,k])
+    
+    def plot(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111,projection="3d")
+        ax.plot(self.xpoint,self.ypoint,self.zpoint,label = "3D Line")
+        ax.legend()
+        ax.set_xlabel('X Axis')
+        ax.set_ylabel('Y Axis')
+        ax.set_zlabel('Z Axis')
+        plt.show()
+
+v = ThreeDVector(1,2,3)
+v.plot()
